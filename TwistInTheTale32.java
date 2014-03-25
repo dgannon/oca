1341d48ccd88f9dc01a4582744f54cfd8cb56066
Dustin Gannon <DustinGannon@gmail.com>
2014-03-24 22:55:11 -0700

    Example code to demoinstrate recursive constructors calls are not going to compile.

---
+++ TwistInTheTale32.java
@@ -0,0 +1,17 @@
+class Employee {
+    String name;
+    int age;
+    
+    Employee() {
+        this();
+    }
+    
+    Employee (String newName, int newAge) {
+        name = newName;
+     	age = newAge;
+    }
+}
+
+class TwistInTheTale32 {
+    Employee x = new Employee();
+}
