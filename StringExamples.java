498301499f735acf27188a0ca5c7933defbe9f77
Dustin Gannon <DustinGannon@gmail.com>
2014-03-26 22:18:47 -0700

    This is a checkin that shows example methods used by the String object.
    It is showing how these methods are used and their paramters and quirks!

--- StringExamples.java
+++ StringExamples.java
@@ -24,5 +24,38 @@ class StringExamples {
     String amIEqualString4 = "Testing if these are the same object";
     System.out.println("Is amIEqualString3 the same object as amIEqualString4 ");
     System.out.println( amIEqualString3==amIEqualString4);
+
+    StringMethodExamples();
+    }
+
+    public static void StringMethodExamples(){
+    String testString = "  This is a String object's methods    ";
+    System.out.println("testString = " + testString);
+
+    //charAt
+    System.out.println("The charAt at index 3 in testString is " +
+                       testString.charAt(3));
+
+    //indexOf
+    System.out.println("The indexOf m in testString is " +
+                       testString.indexOf('m'));
+    System.out.println("The results of of indexOf z in testString is " +
+                     testString.indexOf("indexOfTakesCharAndStringArguments"));
+    //substring
+    System.out.println("The results of substring(2,8) is |" +
+                       testString.substring(2,8) +
+                       " | notice that it grabbed positions upto but not " +
+                       "including position 8!!!");
+    //trim
+    System.out.println("Trim cuts off whitespace from front and back! " +
+                       testString.trim());
+    //length
+    System.out.println("Length is a Method (not field!) testString is " +
+                       testString.length() + " characters long");
+    //startsWith & endsWith
+    System.out.println("startsWith allows you to boolean test to see if a " +
+                       "String starts or ends with a value.");
+    System.out.println("Does testString start and end with a blank?");
+    System.out.println(testString.startsWith(" ") && testString.endsWith(" "));
     }
 }
