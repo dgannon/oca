9b2fe57a97fe022f262a39f12417ab7a9be6529a
Dustin Gannon <DustinGannon@gmail.com>
2014-03-29 13:09:13 -0700

    Example Use of the ArrayList Class with adding and accessing items using an enhanced for loop.

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -4,7 +4,19 @@ class ArrayListExample {
 
     public static void main(String[] args){
 
-    ArrayList<String> myArrayList = new ArrayList<String>();
+        System.out.println("Example class that demoinstrates the ArrayList class");
 
+        ArrayList<String> myArrayList = new ArrayList<String>();
+
+        //How to add items to an ArrayList object.
+        myArrayList.add("one");
+        myArrayList.add("two");
+        myArrayList.add("four");
+        myArrayList.add(2, "three");
+
+        //How to Access elements of an ArrayList Object
+        for (String element : myArrayList){
+            System.out.println(element);
+        }
     }
 }
