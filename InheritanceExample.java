6b153318ca8352f825a70060c5e577a57ed7c189
Dustin Gannon <DustinGannon@gmail.com>
2014-03-30 15:10:58 -0700

    Example code of a interface being used to selectivly give methods to classes in the program.

--- InheritanceExample.java
+++ InheritanceExample.java
@@ -6,6 +6,10 @@ class InheritanceExample{
         Employee myEmployee = new Employee();
         Programmer myProgrammer = new Programmer();
         Manager myManager = new Manager();
+
+        myProgrammer.attendTraining();
+        myManager.conductInterview();
+        myManager.attendTraining();
     }
 }
 
@@ -19,18 +23,38 @@ class Employee {
     float experience;
 }
 
-class Programmer extends Employee {
+class Programmer extends Employee implements Trainable{
     Programmer() {
         System.out.println("Object Created - Programmer");
     }
     String[] programmingLanguages;
     void writeCode() {}
+
+    public void attendTraining(){
+        System.out.println("Prog - attendTraining");
+    }
 }
 
-class Manager extends Employee {
+class Manager extends Employee implements Interviewer, Trainable{
     Manager() {
         System.out.println("Object Created - Manager");
     }    
     int teamSize;
     void reportProjectStatus() {}
+
+    public void conductInterview(){
+        System.out.println("Mgr - conductInterview");
+    }
+
+    public void attendTraining(){
+        System.out.println("Mgr - attendTraining");
+    }
+}
+
+interface Trainable {
+    public void attendTraining();
+}
+
+interface Interviewer{
+    public void conductInterview();
 }
